60aaa588f95fef258b6dc375256b0a275ce19a01
Lucas <mariolucas.2000@outlook.com>
2022-11-01 10:03:38 -0300

    :sparkles: feat: import React hook form and yup

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,52 @@
-import './global.css'
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+import styles from './form.module.css';
+
+
+const schema = yup.object({
+
+  nome: yup.string().required("Nome obrigatorio"),
+  email: yup.string().required("Email obrigatorio").email("E-mail invalido"),
+  password: yup.string().required("Senha obrigatorio").min(6,"No minimo 6 caracteres")
+
+}).required();
+
 
 function App() {
 
+  const { register, handleSubmit , formState: { errors } } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const onSubmit = () => {
+        console.log("ok")
+  }
+
 
   return (
-    <form onSubmit>
-      <div>
-        <input type="text" placeholder='insira seu nome' />
+    <form  onSubmit={handleSubmit(onSubmit)} className={styles.contentForm} >
+      
+      <div className={styles.formGroup}>
+        <input type="text" placeholder='insira seu nome' {...register("nome")} />
       </div>
-      <div>
-        <input type="email" placeholder='insira seu email' />
+      <span>{errors.nome?.message}</span>
+      
+      <div className={styles.formGroup}>
+        <input type="email" placeholder='insira seu email' {...register("email")} />
       </div>
-      <div>
-        <input type="password" placeholder='insira sua senha ' />
+      <span>{errors.nome?.message}</span>
+
+      <div className={styles.formGroup}> 
+        <input type="password" placeholder='insira sua senha ' {...register("password")} />
       </div>
-      <div>
-        <input type="tpassword" placeholder='Confirma sua senha' />
+      <span>{errors.nome?.message}</span>
+
+      <div className={styles.formGroup}>
+        <input type="password" placeholder='Confirma sua senha'  />
       </div>
+      <span>{errors.nome?.message}</span>
 
       <button type='submit'>Enviar formulario</button>
        
